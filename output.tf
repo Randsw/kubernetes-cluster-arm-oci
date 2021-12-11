0046c1e6109ec18ced193cc129eb7742e2d9954d
Randsw <randsw@mail.ru>
2021-12-11 22:48:22 +0300

    Create hosts file for ansible using terraform output. Add new cluster files version

--- output.tf
+++ output.tf
@@ -21,3 +21,21 @@ output "worker1-public-ip"{
 output "worker2-public-ip"{
     value = module.kube_worker_instance_flex.public_ip[1]
 }
+
+output "lb-public-ip"{
+    value = oci_load_balancer_load_balancer.kube_load_balancer.ip_address_details.ip_address
+}
+
+locals {
+  inventory_rendered_content = templatefile("${path.module}/inventory_ansible.tftpl", 
+  {
+    k8s_control_plane_ip = "${module.kube_cp_instance_flex.public_ip[0]}"
+    k8s_worker1_ip = "${module.kube_worker_instance_flex.public_ip[0]}"
+    k8s_worker2_ip = "${module.kube_worker_instance_flex.public_ip[1]}"
+  })
+}
+
+resource "local_file" "inventories" {
+  content = local.inventory_rendered_content
+  filename = "${path.module}/ansible/inventories/k8s-cluster/hosts.yml"
+}
